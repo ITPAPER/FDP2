9b6939232117bbbaddf34b34ab9564ddac1bf0ed
kihyub <nobinson3@gmail.com>
2019-11-14 20:10:20 +0900

    [박기협]컨트롤러 오류 수정

--- src/main/java/fdp/project/spring/controllers/Controller_K.java
+++ src/main/java/fdp/project/spring/controllers/Controller_K.java
@@ -1,6 +1,5 @@
 package fdp.project.spring.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,27 +7,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class Controller_K {
 	
-	/** WebHelper 주입 */
-	@Autowired
-	WebHelper webHelper;
-	
-	/** RegexHelper 주입 */
-	@Autowired
-	RegexHelper regexHelper;
-	
 	@RequestMapping(value = "21_Management.do", method = RequestMethod.GET)
 	public String Management() {
 
 		return "21_Management";
 	}
 
-	@RequestMapping(value = "22_Login_s.do", method = RequestMethod.POST)
+	@RequestMapping(value = "22_Login_s.do", method = RequestMethod.GET)
 	public String Login_s() {
 
 		return "22_Login_s";
 	}
 
-	@RequestMapping(value = "26_Profile_i.do", method = RequestMethod.POST)
+	@RequestMapping(value = "26_Profile_i.do", method = RequestMethod.GET)
 	public String Profile_i() {
 
 		return "26_Profile_i";
